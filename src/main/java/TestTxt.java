533262edeabc5871481605d0d72f64b5684ea3f6
timmy <1301428882@qq.com>
2019-06-18 19:53:42 +0800

    。。

--- src/main/java/TestTxt.java
+++ src/main/java/TestTxt.java
@@ -1,20 +1,25 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2019/6/13.
  */
 public class TestTxt {
 
-    public static void readWantedText(String url, String wanted) {
+
+    public static String readWantedText(String url, String wanted) {
+        String temps = "" ;
         try {
             InputStreamReader fr = new InputStreamReader(new FileInputStream(url),"gbk");
             BufferedReader br = new BufferedReader(fr);
             String temp = "";// 用于临时保存每次读取的内容
+
             while (temp != null) {
                 temp = br.readLine();
-                System.out.println(temp);
+//                System.out.println(temp);
                 if (temp != null && temp.contains(wanted)) {
-                    System.out.println(temp);
+                   temps= temp;
                 }
             }
         } catch (FileNotFoundException e) {
@@ -24,10 +29,21 @@ public class TestTxt {
 // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        return temps;
     }
 
     public static void main(String[] args){
-       TestTxt.readWantedText("C:\\Users\\Administrator\\Downloads\\三日重复数据查询.txt","我爱你");
+
+        File file=new File("d:\\文档");
+        final List<File> filePathsList = new ArrayList<File>();
+        File[] filePaths = file.listFiles();
+//        System.out.println(filePaths[0]+"  11111");
+        for (File s : filePaths) {
+        String  aa=TestTxt.readWantedText(s.toString(),"我爱你");
+        System.out.println(aa+" 11");
+        }
+
+//       TestTxt.readWantedText("C:\\Users\\Administrator\\Downloads\\三日重复数据查询.txt","我爱你");
 
     }
 }
